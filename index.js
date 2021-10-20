a394c65b845a68a18bc2e6d9795051abc2ef68fa
florcarpanetto <84979891+florcarpanetto@users.noreply.github.com>
2021-10-19 23:22:47 -0300

    division

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const suma = (a, b) => a+b;
 const resta = (a, b) => a-b;
+
+const division = (a, b) => {
+    try {a/b}
+    catch(err) {
+        console.err(err)
+    }
+    finally {return 0}
+};
