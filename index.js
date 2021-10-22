7150ca54d192d52e1dc8a24c19bbe5db3342de3a
florcarpanetto <florcarpanetto@gmail.com>
2021-10-21 21:45:29 -0300

    porcentaje

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const suma = (a, b) => a+b;
-const resta = (a, b) => a-b;
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
 
 const division = (a, b) => {
-    try {a/b}
-    catch(err) {
+    try { a / b }
+    catch (err) {
         console.err(err)
     }
-    finally {return 0}
+    finally { return 0 }
 };
 
-const promedio = suma /2;
+const promedio = suma / 2;
 
-const multiplicacion = (a, b) => a*b;
+const multiplicacion = (a, b) => a * b;
+
+const porcentaje = (a, b) => b*100/a
